5593efdf8671760dcbb0b36f3993c3642529ffe6
Julian Garcia <julian.gabus@gmail.com>
2018-02-16 18:02:05 -0600

    optimized nested lookup

---
+++ excel.py
@@ -0,0 +1,34 @@
+from openpyxl import load_workbook
+from openpyxl import Workbook
+from openpyxl.compat import range
+from openpyxl.utils import get_column_letter
+from openpyxl.workbook.views import BookView
+
+def write_to_excel(input_txt, output_xlsx):
+    with open(input_txt + '.txt', 'r') as input_file:
+        with open('temp.txt', 'a+') as temp_file:
+            row = 1
+            for line in input_file:
+                final_string = ''
+                for field in line.split('"'):
+                    field = field.strip()
+                    field += '"'
+                    final_string += field
+
+                temp_file.write(final_string + "\n")
+                row += 1
+
+
+    with open('temp.txt', 'r') as input_file:
+        wb = Workbook()
+        ws = wb.active
+        row = 1
+
+        for line in input_file:
+            for index, field in enumerate(line.split('"')):
+                print(row)
+                ws[get_column_letter(index+1) + str(row)] = field
+            row += 1
+        wb.save(output_xlsx + '.xlsx')
+
+write_to_excel('violations', 'excel')
--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+import time
+import traceback
 from pyscraper.selenium_utils import get_headed_driver, get_headless_driver, wait_for_classname, wait_for_id, wait_for_tag
 from pyscraper.data_dump_file import DataFile
 
 driver = get_headless_driver(no_sandbox=True)
 try:
     driver.get("http://jp-appserver.jeffparish.net/servicerequest/findServices.aspx")
+    time.sleep(5)
     links = driver.find_elements_by_tag_name('a')
     address = [link for link in links if 'Address' in link.text][0]
     address.click()
@@ -11,10 +14,10 @@ try:
     street = wait_for_id(driver, 'ddlStreet', time=20)
     options = street.find_elements_by_tag_name('option')
 
-    output = DataFile('violations')
-    log = DataFile('log1.2')
+    output = DataFile('violations_final')
+    log = DataFile('logger')
     with output, log:
-        for index in range(56, len(options)):
+        for index in range(58, len(options)):
             street = wait_for_id(driver, 'ddlStreet')
             options = street.find_elements_by_tag_name('option')
             options[index].click()
@@ -24,75 +27,71 @@ try:
             rows = driver.find_elements_by_tag_name('tr')
             log.load_value(street + " rows: " + str(len(rows)))
             repeat_offenders = 0
-            print '\n' + street, len(rows)
-            for i in range(len(rows)):
+            scripts = []
+            for row in rows:
+                if 'repeat offender' in row.text.lower() and ('/2018' in row.text or '/2017' in row.text or '/2016' in row.text):
+                    link = row.find_element_by_tag_name('a')
+                    first_section = row.text.split()
+                    # service_id = first_section[0]
+                    # item_number = first_section[1]
+                    # type = first_section[2] + " " + first_section[3]
+                    # location = first_section[4] + " " + first_section[5] + " " + first_section[6] + " " + first_section[7]
+                    # service_requested = first_section[8]
+                    scripts.append((first_section, link.get_attribute('href')))
+                    print link
+            print 'scripts'
+            for first_section, script in scripts:
+                driver.execute_script(script)
                 try:
-                    print i,
-                    wait_for_classname(driver, 'special_alt_td')
-                    rows = driver.find_elements_by_tag_name('tr')
-                    try:
-                        row = rows[i]
-                    except:
-                        try:
-                            rows = driver.find_elements_by_tag_name('tr')
-                            row = rows[i]
-                        except:
-                            print 'NOT ENOUGH ROWS HEH'
-                            continue
-                    test_text = [element.text for element in row.find_elements_by_tag_name('tr')]
-                    row_text = row.text
-                    first_section = row_text.split()
-                    if 'repeat offender' in row_text.lower() and ('/2018' in row_text or '/2017' in row_text):
-                        repeat_offenders += 1
-                        print '\n' + row_text
-                        link = row.find_element_by_tag_name('a')
-                        link.click()
-                        wait_for_classname(driver, 'lbl_title')
-                        second_section = [element.text for element in driver.find_elements_by_class_name('lbl_title')]
+                    wait_for_classname(driver, 'lbl_title')
 
-                        service_id = first_section[0]
-                        item_number = first_section[1]
-                        type = first_section[2] + " " + first_section[3]
-                        location = first_section[4] + " " + first_section[5] + " " + first_section[6] + " " + first_section[7]
-                        service_requested = first_section[8]
+                    second_section = [element.text for element in driver.find_elements_by_class_name('lbl_title')]
 
-                        output.load_values(service_id, item_number, type, location, service_requested)
+                    service_id = first_section[0]
+                    item_number = first_section[1]
+                    type = first_section[2] + " " + first_section[3]
+                    location = ''
+                    for section in first_section[4:-1]:
+                        location += section + " "
+                    service_requested = first_section[-1]
 
-                        complaint_type = second_section[5]
-                        complaint_dept = second_section[7]
-                        received_date = second_section[9]
-                        subdivision = second_section[11]
-                        address = second_section[13]
-                        city = second_section[15]
-                        bank_of_river = second_section[17]
+                    output.load_values(service_id, item_number, type, location, service_requested)
 
-                        output.load_values(complaint_type, complaint_dept, received_date, subdivision, address, city, bank_of_river)
+                    complaint_type = second_section[5]
+                    complaint_dept = second_section[7]
+                    received_date = second_section[9]
+                    subdivision = second_section[11]
+                    address = second_section[13]
+                    city = second_section[15]
+                    bank_of_river = second_section[17]
 
-                        activity_table = driver.find_element_by_class_name('special_table')
-                        table_rows = activity_table.text.split('\n')
-                        activity_information = []
-                        # log.write_values(street, len(table_rows))
-                        for trow in table_rows[1:]:
-                            type_of_activity = ''
-                            for word in trow.split()[:-1]:
-                                type_of_activity += word + " "
-                            date_of_activity = trow.split()[-1]
-                            type_of_activity = type_of_activity.strip()
-                            activity_information.append((type_of_activity, date_of_activity))
+                    output.load_values(complaint_type, complaint_dept, received_date, subdivision, address, city, bank_of_river)
 
-                        for activity in activity_information:
-                            type_of_activity, date_of_activity = activity
+                    activity_table = driver.find_element_by_class_name('special_table')
+                    table_rows = activity_table.text.split('\n')
+                    activity_information = []
+                    for trow in table_rows[1:]:
+                        type_of_activity = ''
+                        for word in trow.split()[:-1]:
+                            type_of_activity += word + " "
+                        date_of_activity = trow.split()[-1]
+                        type_of_activity = type_of_activity.strip()
+                        activity_information.append((type_of_activity, date_of_activity))
 
-                            output.load_values(type_of_activity, date_of_activity)
+                    for activity in activity_information:
+                        type_of_activity, date_of_activity = activity
 
-                        output.write_loaded()
-                        driver.back()
-                    else:
-                        continue
+                        output.load_values(type_of_activity, date_of_activity)
+
+                    output.write_loaded()
+                    print 'written'
                 except:
+                    print 'something'
+                    traceback.print_exc()
                     continue
-            log.load_value(str(repeat_offenders))
-            log.write_loaded()
+                finally:
+                    driver.back()
+
             driver.back()
 finally:
     driver.close()
