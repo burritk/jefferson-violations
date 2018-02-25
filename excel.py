808227a935cc364f0c9687d560f5214f2ab108be
Julian Garcia <julian.gabus@gmail.com>
2018-02-25 04:01:28 -0600

    full_data

--- excel.py
+++ excel.py
@@ -5,21 +5,21 @@ from openpyxl.utils import get_column_letter
 from openpyxl.workbook.views import BookView
 
 def write_to_excel(input_txt, output_xlsx):
+    # with open(input_txt + '.txt', 'r') as input_file:
+    #     with open('temp.txt', 'a+') as temp_file:
+    #         row = 1
+    #         for line in input_file:
+    #             final_string = ''
+    #             for field in line.split('"'):
+    #                 field = field.strip()
+    #                 field += '"'
+    #                 final_string += field
+    #
+    #             temp_file.write(final_string + "\n")
+    #             row += 1
+
+
     with open(input_txt + '.txt', 'r') as input_file:
-        with open('temp.txt', 'a+') as temp_file:
-            row = 1
-            for line in input_file:
-                final_string = ''
-                for field in line.split('"'):
-                    field = field.strip()
-                    field += '"'
-                    final_string += field
-
-                temp_file.write(final_string + "\n")
-                row += 1
-
-
-    with open('temp.txt', 'r') as input_file:
         wb = Workbook()
         ws = wb.active
         row = 1
@@ -31,4 +31,4 @@ def write_to_excel(input_txt, output_xlsx):
             row += 1
         wb.save(output_xlsx + '.xlsx')
 
-write_to_excel('violations', 'excel')
+write_to_excel('full_data', 'full_data')
